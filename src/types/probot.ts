fbb34a594aedd0b47250fb2d90ed85a4a0437520
Eric Ribeiro <ericribeiro@outlook.com.br>
2021-02-14 11:55:57 -0800

    added new properties to created type

--- src/types/probot.ts
+++ src/types/probot.ts
@@ -1,6 +1,9 @@
 export type MetabotWebhookPayloadPullRequest = import('@octokit/webhooks/dist-types/generated/event-payloads')
     .EventPayloads
-    .WebhookPayloadPullRequest
+    .WebhookPayloadPullRequest & {
+        botComment: String,
+        botName: String
+    }
 
 export type MetabotWebhookEvents = import('@octokit/webhooks/dist-types/generated/get-webhook-payload-type-from-event')
     .WebhookEvents
